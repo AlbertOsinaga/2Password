d5e50e8301f8b8199b6a6a5fc176115ef22cb54b
AlbertOsinaga <luis.osinaga@gmail.com>
2020-08-08 12:16:58 -0400

    Setup inicial

---
+++ 2PwdSolution/2PwdClasses/Global.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+namespace _2PwdClasses
+{
+    public static class Global
+    {
+        public static void WriteHola() => WriteLine("Hola desde Global!");
+    }
+}
---
+++ 2PwdSolution/2PwdConsola/Programa.cs
@@ -0,0 +1,22 @@
+﻿#region Header
+using System;
+using static System.Console;
+using _2PwdClasses;
+
+namespace _2PwdConsola
+{
+#endregion
+   
+    static class Programa
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello 2Pwd!");
+            Global.WriteHola();
+            ReadKey();
+        }
+    }
+
+#region Footer
+}
+#endregion
