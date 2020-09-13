e904fad417e238c1f8be8aa28fc392667b99b7b0
AlbertOsinaga <luis.osinaga@gmail.com>
2020-09-13 17:34:40 -0400

    PC.Parse Ok version Ok

--- 2PwdSolution/2PwdClasses/ProcesadorComandos.cs
+++ 2PwdSolution/2PwdClasses/ProcesadorComandos.cs
@@ -4,6 +4,8 @@ using System;
 using PC = _2PwdClasses.ProcesadorComandos;
 using MR = _2PwdClasses.ManejadorRegistros;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _2PwdClasses
 {
@@ -81,12 +83,12 @@ namespace _2PwdClasses
             {
                 regComando.Cmd = lineaCmd.Substring(0, i).Trim().ToLower();
                 if (i + 1 >= 4)
-                    regComando.Arg = lineaCmd.Substring(i + 1); 
+                    regComando.Arg = lineaCmd.Substring(i + 1);
             }
-            if(regComando.Cmd.Length != 3)
+            if (regComando.Cmd.Length != 3)
             {
                 PC.HayError = true;
-                PC.MensajeError = $"Error: Comando '{regComando.Cmd}' debe ser de 3 caracteres," + 
+                PC.MensajeError = $"Error: Comando '{regComando.Cmd}' debe ser de 3 caracteres," +
                                     $" en {nameof(ProcesadorComandos)}.{nameof(Parse)}!";
                 return regComando;
             }
@@ -101,72 +103,79 @@ namespace _2PwdClasses
                     return Parse(regComando);
                 default:
                     PC.HayError = true;
-                    PC.MensajeError = $"Error: comando '{regComando.Cmd}' no reconcido, " + 
+                    PC.MensajeError = $"Error: comando '{regComando.Cmd}' no reconcido, " +
                                         $"en {nameof(ProcesadorComandos)}.{nameof(Parse)}!";
                     return regComando; ;
             }
+
+            #region Funciones
 
             Comando Parse(Comando regCmd)
             {
                 switch (regComando.Cmd)
                 {
                     case "add":
-                        return ParseAdd(regCmd);
                     case "del":
                     case "get":
                     case "lst":
                     case "upd":
+                        return ParseCmd(regCmd);
                     default:
                         PC.HayError = true;
-                        PC.MensajeError = $"Error: comando '{regComando.Cmd}' no reconcido, " +
+                        PC.MensajeError = $"Error: comando '{regCmd.Cmd}' no reconcido, " +
                                             $"en {nameof(ProcesadorComandos)}.{nameof(Parse)}!";
                         return regCmd;
                 }
             }
 
-            Comando ParseAdd(Comando regCmd)
+            Comando ParseCmd(Comando regCmd)
             {
                 //
-                // Este metodo arma el argumento en una row normalizada de la forma
+                // Este metodo arma el argumento del cmd add en una row normalizada de la forma
                 // UserNombre|Categoria|Empresa|Producto|Numero|WebEmpresa|UserId|UserPwd|UserEmail|userNotas|
                 //
 
-                string arg = "";
-                int ixUno = regCmd.Arg.IndexOf("uno:"); // UserNombre
-                if(ixUno >= 0)
+                var cmds = new string[] {};
+                switch(regCmd.Cmd)
                 {
-                    ixUno += 4;
-                    if(ixUno < regCmd.Arg.Length)
-                    {
-                        int endUno = ixUno;
-                        if(regCmd.Arg[ixUno] == '\"')
-                        {
-                            ixUno++;
-                            endUno = regCmd.Arg.IndexOf("\"", ixUno);
-                            if(endUno > ixUno)
-                            {
-                                arg += regCmd.Arg.Substring(ixUno, endUno - ixUno);
-                                regCmd.Arg = arg;
-                                regCmd.Ok = true;
-                            }
-                        }
-                        else
-                        {
-                            endUno = regCmd.Arg.IndexOf(" ", ixUno);
-                            endUno = endUno > 0 ? endUno : regCmd.Arg.Length;
-                            if (endUno > ixUno)
-                            {
-                                arg += regCmd.Arg.Substring(ixUno, endUno - ixUno);
-                                regCmd.Arg = arg;
-                                regCmd.Ok = true;
-                            }
-                        }
-
-                    }
+                    case "add":
+                        cmds = new[] { "nom", "cat", "emp", "cta", "nro", "web", "uid", "pwd", "ema", "not" };
+                        break;
+                    case "del":
+                    case "get":
+                    case "lst":
+                        cmds = new[] { "nom", "cat", "emp", "cta", "nro" };
+                        break;
+                    case "upd":
+                        cmds = new[] { "nom", "cat", "emp", "cta", "nro", "web", "uid", "pwd", "ema", "not", "fcr", "fup", "rid" };
+                        break;
+                    default:
+                        break;
                 }
 
+                var partes = regCmd.Arg.Split('-');
+                var campos = new Dictionary<string, string>();
+                foreach (var campo in partes)
+                {
+                    if (campo.Length > 4 && cmds.Where(cmd => cmd == campo.Substring(0, 3)).Any() && campo[3] == ' ')
+                        campos[campo.Substring(0, 3)] = campo.Substring(4).Trim();
+                }
+
+                var arg = "";
+                foreach (var cmd in cmds)
+                {
+                    if (campos.ContainsKey(cmd))
+                        arg += campos[cmd];
+                    arg += cmd == cmds[cmds.Length - 1] ? "" : "|";
+                }
+
+                regCmd.Arg = arg;
+                regCmd.Ok = true;
                 return regCmd;
             }
+
+
+            #endregion
         }
 
 
@@ -182,7 +191,7 @@ namespace _2PwdClasses
         //    switch (regComando.Cmd)
         //    {
         //        case "add":
-        //            respuesta = PC.AddPwds(regComando.Arg);
+        //            respuesta = PC.AddPwds(regComando.Arg);https://api.taboola.com/2.0/json/msn-anaheim-us/recommendations.notify-click?app.type=desktop&app.apikey=dd914485a5ed62ec1086bc1f372e410b851bc1e0&response.id=__57dbf6376eb42ccc5f4fcae52edb7879__ea64cd5a5101de84268a61dd6b307d65&response.session=v2_0afbed84ab22413805f7a94cf9362b7d_109624A5BCFD6E122D462A5FBDE96FC1_1599918471_1599918471_CIi3jgYQsORKGMz669e4ipfamgEgASgFMB44htMHQOCHEEi02dgDUP___________wFYAGAAaJHhgryfr6Tf2QE&item.id=%7E%7EV1%7E%7E-1206419116654905771%7E%7ELapUE77QL7-UbwMbvLG3OfBnf6AgXMqtwCFBTr-GeVYndpXq_nTToVci-tV_1bYyPVPbFHdycXfyr1VxmozLcV7JbGFbjtizQN29Zpin8p1JlawpbYFtoRw_FVSAoDPy8yoVX_ZV1DUzrU9mgEqIxUUWQYJbJAZQ_PxdsyidTxwHevLFaRMtkz52WhIKlmx7-csEBSsOEU6ufprwD-3fSi9Ezl800pG9duHuzBPjTUSAhDH4QnJoSS1idJX-Rd20Sh1b8LRPczWnT2TkP21u0fHnd3F8OVk2bR-QZRW3RyT-S5sN_yP-AY0beD0JthKK&item.type=text&sig=119fe907cc58e7c19f4e32bc2d3a55265ca250c5dc4f&redir=https%3A%2F%2Fwww.vpnmentor.com%2Fpopular%2Fwatch-disney-plus-online-anywhere%2F%3Futm_source%3Dtaboola%26utm_medium%3Dpaid%26tblcid%3DGiAc8YD07K6TzKrK-qmzpcVlVWEr6Ia40h5EYeyoYNdQHyCs8U4%26campaign%3DDisney%2BPlus%2B-%2BPremium%2B-%2BNew%26campaignid%3D5755473%26site%3Dmsn-anaheim-us%26siteid%3D1225264%26title%3DUnblock%2BDisney%2BPlus%2BFrom%2BBolivia%2521%26image%3Dhttp%253A%252F%252Fcdn.taboola.com%252Flibtrc%252Fstatic%252Fthumbnails%252FGETTY_IMAGES%252FSKP%252F1032516774__UGubyCf8.jpg%26accountid%3D1292460%26ad%3D2918819387%26tblci%3DGiAc8YD07K6TzKrK-qmzpcVlVWEr6Ia40h5EYeyoYNdQHyCs8U4%23tblciGiAc8YD07K6TzKrK-qmzpcVlVWEr6Ia40h5EYeyoYNdQHyCs8U4&ui=109624A5BCFD6E122D462A5FBDE96FC1
         //            break;
         //        case "list":
         //            respuesta = PC.ListPwds();
--- 2PwdSolution/2PwdTests/PC_Tests.cs
+++ 2PwdSolution/2PwdTests/PC_Tests.cs
@@ -17,46 +17,125 @@ namespace _2PwdTests
     
     public class PC_Tests
     {
+       //[Fact]
+       //public void Test1()
+       // {
+       //     // Prepara
+       //     var linea = "add -nom Luis Alberto -cat cat Software -emp Microsoft -cta Azure -nro Cuenta 1 " +
+       //                 " -web web.azure.com -uid laos -pwd clavesecreta -ema luis.@gmail.com -not prueba de Registro";
+
+       //     // Ejecuta
+       //     PC.Parse(linea);
+
+       //     // Prueba
+
+       // }
+        
         [Fact]
-        public void Parse_add_uno_ok_comillas()
+        public void Parse_add_ok_espacios()
         {
             // Prepara
-            var lineaComando = "add uno:\"Juan Perez\"";
+            var lineaComando = "add -nom Juan Perez -uid jperez -pwd secreto!";
+
             // Ejecuta
             var regCmd = PC.Parse(lineaComando);
 
             // Prueba
             Assert.True(regCmd.Ok);
             Assert.Equal("add", regCmd.Cmd);
-            Assert.Equal("Juan Perez", regCmd.Arg);
+            Assert.Equal("Juan Perez||||||jperez|secreto!||", regCmd.Arg);
         }
 
         [Fact]
-        public void Parse_add_uno_ok_noComillas_endLine()
+        public void Parse_add_ok_full()
         {
             // Prepara
-            var lineaComando = "add uno:Juan_Perez";
+            var lineaComando = "add -cta Microsoft Azure -nro #123001 -nom Luis Alberto -cat Software -emp Microsoft " +
+                        " -web web.azure.com -uid laos -pwd supersecreta -ema luis.@gmail.com -not prueba de registro";
+
             // Ejecuta
             var regCmd = PC.Parse(lineaComando);
 
             // Prueba
             Assert.True(regCmd.Ok);
             Assert.Equal("add", regCmd.Cmd);
-            Assert.Equal("Juan_Perez", regCmd.Arg);
+            Assert.Equal("Luis Alberto|Software|Microsoft|Microsoft Azure|#123001|web.azure.com|laos|supersecreta|luis.@gmail.com|prueba de registro", regCmd.Arg);
         }
 
         [Fact]
-        public void Parse_add_uno_ok_noComillas_espacio()
+        public void Parse_add_ok_noEspacios()
         {
             // Prepara
-            var lineaComando = "add uno:Juan Perez";
+            var lineaComando = "add -cta Camaleon -uid carlos.lopez -pwd facil_clave -web www.camaleon.com -not todo es igual!";
             // Ejecuta
             var regCmd = PC.Parse(lineaComando);
 
             // Prueba
             Assert.True(regCmd.Ok);
             Assert.Equal("add", regCmd.Cmd);
-            Assert.Equal("Juan", regCmd.Arg);
+            Assert.Equal("|||Camaleon||www.camaleon.com|carlos.lopez|facil_clave||todo es igual!", regCmd.Arg);
+        }
+
+        [Fact]
+        public void Parse_del_ok_full()
+        {
+            // Prepara
+            var lineaComando = "del -nom Luis Alberto -cat Software -emp Microsoft -cta Microsoft Azure -nro 0001";
+
+            // Ejecuta
+            var regCmd = PC.Parse(lineaComando);
+
+            // Prueba
+            Assert.True(regCmd.Ok);
+            Assert.Equal("del", regCmd.Cmd);
+            Assert.Equal("Luis Alberto|Software|Microsoft|Microsoft Azure|0001", regCmd.Arg);
+        }
+
+        [Fact]
+        public void Parse_get_ok_full()
+        {
+            // Prepara
+            var lineaComando = "get -nom Luis Alberto -cat Software -emp Microsoft -cta Microsoft Azure -nro 0001";
+
+            // Ejecuta
+            var regCmd = PC.Parse(lineaComando);
+
+            // Prueba
+            Assert.True(regCmd.Ok);
+            Assert.Equal("get", regCmd.Cmd);
+            Assert.Equal("Luis Alberto|Software|Microsoft|Microsoft Azure|0001", regCmd.Arg);
+        }
+
+        [Fact]
+        public void Parse_lst_ok_full()
+        {
+            // Prepara
+            var lineaComando = "lst -nom Luis Alberto -cat Software -emp Microsoft -cta Microsoft Azure -nro 0001";
+
+            // Ejecuta
+            var regCmd = PC.Parse(lineaComando);
+
+            // Prueba
+            Assert.True(regCmd.Ok);
+            Assert.Equal("lst", regCmd.Cmd);
+            Assert.Equal("Luis Alberto|Software|Microsoft|Microsoft Azure|0001", regCmd.Arg);
+        }
+
+        [Fact]
+        public void Parse_upd_ok_full()
+        {
+            // Prepara
+            var lineaComando = "upd -cta Microsoft Azure -nro #123001 -nom Luis Alberto -cat Software -emp Microsoft " +
+                        " -web web.azure.com -uid laos -pwd supersecreta -ema luis.@gmail.com -not prueba de registro " +
+                        "-fcr -fup -rid";
+
+            // Ejecuta
+            var regCmd = PC.Parse(lineaComando);
+
+            // Prueba
+            Assert.True(regCmd.Ok);
+            Assert.Equal("upd", regCmd.Cmd);
+            Assert.Equal("Luis Alberto|Software|Microsoft|Microsoft Azure|#123001|web.azure.com|laos|supersecreta|luis.@gmail.com|prueba de registro|||", regCmd.Arg);
         }
     }
 
